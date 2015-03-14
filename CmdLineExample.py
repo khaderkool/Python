7d1fbfd3ae6b9503a9eeca8773f2bd580f489f45
khaderkool <khader.kool@gmail.com>
2015-03-14 07:34:49 +0530

    Changing into more redable format

--- CmdLineExample.py
+++ CmdLineExample.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 
+# import comand line related libraries
+
 import sys, getopt
+
+#helper functions
+
 def Usage():
 	print 'test.py -i <inputfile> -o <outputfile>'
 	sys.exit()
+
+# main function
 def main(argv):
-	inputfile = ''
-	outputfile = ''
-#	while(1):
+	#start caring for exceptions
 	try:
 		print "len(sys.argv) " + str(len(sys.argv))
+		# make sure that we have 5 args 1)filename 2) -i 3) filename 4) -o 5) filename
 		if(len(sys.argv) != 5):
 			print 'Usaage: test.py -i <inputfile> -o <outputfile>'
 			sys.exit()
@@ -18,21 +24,21 @@ def main(argv):
 		print 'test.py -i <inputfile> -o <outputfile>'
 		Usage()
 		sys.exit(2)
-	print(argv , args)
+#	print(argv , args)
 	for opt, arg in opts:
 		if opt == '-h':
 			Usage()
 		elif opt in ("-i", "--ifile"):
-			print(" -i flag")
-			inputfile = arg
+			print("input file path: " + arg)
+			#inputfile = arg
 		elif opt in ("-o", "--ofile"):
 			outputfile = arg
-			print(" -o flag")
+			print("Output file path: " + arg)
 		else:
 			print ("Usage")
 			Usage()
-	print 'Input file is "', inputfile
-	print 'Output file is "', outputfile
+
+# lets wait for our main function
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
